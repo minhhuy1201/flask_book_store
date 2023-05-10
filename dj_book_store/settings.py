fbaf83d056a312f106bce236bf277655d1b02c62
d3m0l15h <davicmax12@gmail.com>
2023-05-10 20:18:47 +0700

    upload media and file

--- dj_book_store/settings.py
+++ dj_book_store/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 import environ
 from pathlib import Path
 env = environ.Env(
@@ -125,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/home/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'home/media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
