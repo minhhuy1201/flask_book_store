54e1f30447503a307f2cfb8f11d3bdd710330da8
d3m0l15h <davicmax12@gmail.com>
2023-05-11 20:11:10 +0700

    update request database

--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def get_home(request):
     return render(request, 'home.html')
+def get_book(request):
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, 'book.html', context)
