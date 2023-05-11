0c6296f7cbcb413d8630be6b5808fc779313b68a
d3m0l15h <davicmax12@gmail.com>
2023-05-11 20:10:55 +0700

    update url

--- dj_book_store/urls.py
+++ dj_book_store/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home.get_home)
+    path('', home.get_home),
+    path('book/', home.get_book)
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
